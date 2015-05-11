be0225e3986db7811a1faebde556a4efc20c798d
brianjosel <brian.josel@gmail.com>
2015-05-11 13:00:22 -0400

    add comment

--- MemeMe/MemeImage.swift
+++ MemeMe/MemeImage.swift
@@ -13,13 +13,16 @@ struct MemeImage {
     //struct for an image that is Meme'ed
     //Struct has optional top and bottom UITextField from user input that are used in conjunction with UIImge to render new image with UITextFields superimposed on UIImage using method createMeme
     
+    
+    //all variables of MemeImage struct
     //Top and bottom strings, one or more could be optional
     var topText: String?
     var bottomText: String?
+    
     //Image for MemeImage is required
     var origImage: UIImage!
     
-    //output image from method 'createMeme'
+    //output image from method 'createMeme' also required
     var memeImage: UIImage!
     
     func createMeme(topText: String, bottomText: String, image: UIImage) -> UIImage {
