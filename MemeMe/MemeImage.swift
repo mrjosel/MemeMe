81ef1a5c013465bd2e3e5644a2d30c1930920da3
brianjosel <brian.josel@gmail.com>
2015-05-11 17:00:00 -0400

    set view in storyboard to MemeMeViewController initializers added to MemeImage struct

--- MemeMe/MemeImage.swift
+++ MemeMe/MemeImage.swift
@@ -11,22 +11,46 @@ import UIKit
 
 struct MemeImage {
     //struct for an image that is Meme'ed
-    //Struct has optional top and bottom UITextField from user input that are used in conjunction with UIImge to render new image with UITextFields superimposed on UIImage using method createMeme
-    
+    //Struct has optional top and bottom UITextField from user input that are used in conjunction with UIImge to render new image with UITextFields superimposed on UIImage.  Method exists in MemeMeViewController
     
     //all variables of MemeImage struct
     //Top and bottom strings, one or more could be optional
     var topText: String?
     var bottomText: String?
     
-    //Image for MemeImage is required
+    //Image from user, required from user
     var origImage: UIImage!
     
-    //output image from method 'createMeme' also required
-    var memedImage: UIImage!
+    //output image from method 'createMeme'
+    var memedImage: UIImage?
     
-    func createMeme(topText: String, bottomText: String, image: UIImage) -> UIImage {
-        //add code to create new image
-        return memedImage
+    
+    //Initializers
+    init(userTopText: String?, userBottomText: String?, userImage: UIImage?) {
+        //if user entered text for top and bottom strings, set the appropriate variables, else set to empty strings
+        //memedImage is still not defined until MemeMeViewController creates memedImage
+        
+        //top text
+        if let topString = userTopText {
+            self.topText = topString
+        } else {
+            self.topText = ""
+        }
+        //bottom text
+        if let bottomString = userBottomText {
+            self.bottomText = bottomString
+        } else {
+            self.bottomText = ""
+        }
+        //user image
+        self.origImage = userImage
+    }
+    
+        //Initialize with no params, should not ever need this
+    init () {
+        self.topText = ""
+        self.bottomText = ""
+        self.origImage = UIImage()
+        self.memedImage = UIImage()
     }
 }
