a1f2d50d6e7702b2adc7ec53db389c23942ad518
brianjosel <brian.josel@gmail.com>
2015-05-18 20:27:27 -0400

    Collection view finished need to fix VC stack comment and clean code

---
+++ MemeMe/MemeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Brian Josel on 5/18/15.
+//  Copyright (c) 2015 Brian Josel. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var memeCellImageView: UIImageView!
+}
--- MemeMe/SentMemesCollectionViewController.swift
+++ MemeMe/SentMemesCollectionViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class SentMemesCollectionViewController: UIViewController {
+class SentMemesCollectionViewController: UIViewController, UICollectionViewDataSource {
 
+    //array of saved memes
+    var memes : [MemeImage]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //load memes array
+        let object = UIApplication.sharedApplication().delegate
+        let appDelegate = object as! AppDelegate
+        self.memes = appDelegate.memes
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +29,27 @@ class SentMemesCollectionViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //gets size of collection
+        return self.memes.count
     }
-    */
-
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath:NSIndexPath) {
+        let detailController = self.storyboard?.instantiateViewControllerWithIdentifier("MemeDetailViewController") as! MemeDetailViewController
+        detailController.loadedMeme = self.memes[indexPath.row]
+        self.navigationController?.pushViewController(detailController, animated: true)
+    }
+        
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        //let cell = tableView.dequeueReusableCellWithIdentifier("memeCell") as! UITableViewCell
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("memeCollectionCell", forIndexPath: indexPath) as! MemeCollectionViewCell
+        let meme = self.memes[indexPath.row]
+        
+        cell.memeCellImageView.image = meme.memedImage
+        
+        return cell
+    }
+    
+    
+    
 }
